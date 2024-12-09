da219430d2bc0235b7c18cc970a0c136d7558b91
yandorneles <yandorneles05@gmail.com>
2024-12-09 10:31:57 -0300

    Finalizando a atividade 2

--- mysite/aphonso_bet/urls.py
+++ mysite/aphonso_bet/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.pagina_inicial, name="pagina_inicio"),
     path('login/', views.pagina_login, name="pagina_login"),
-    path('logout/', views.pagina_inicial, name="pagina_logout"),
+    path('logout/', views.pagina_logout, name="pagina_logout"),
     path('cadastro/', views.pagina_cadastro, name="pagina_cadastro"),
     path('apostas/', views.pagina_apostas, name="pagina_apostas"),
     path('apostar/<int:jogo_id>', views.pagina_apostar, name="pagina_apostar"),
